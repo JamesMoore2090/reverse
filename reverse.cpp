09a738e1f1929f904b95a3618ee23b035c0b8aac
James Moore <jmoore7@rosemary.umw.edu>
2014-09-05 20:07:18 -0400

    everything is working Minus the directions

--- reverse.cpp
+++ reverse.cpp
@@ -1,6 +1,6 @@
 // James Moore
 // reverse.cpp
-// aug 30
+// sept 5 2014
 // davies
 // this program is designed to read in a file then print out the reverse
 // directions and calculate the estimated gas cost and and the mileage.
@@ -10,7 +10,7 @@
 #include <fstream>
 #include <cmath>
 #include <stdlib.h>
-
+#include <iomanip>
 using namespace std;
 
 const int MAX_NUM_WORDS = 10000;
@@ -27,9 +27,9 @@ string UMW;
 string numDir;
 string destination;
 string location;
-double mile;
-double gasTotal;
-
+double mile=0.00;
+double gasTotal=0.00;
+double mileTotal=0.00;
 	//check to see if the command arg. are valid
 	if(argc != 2){
 		cout << "Usage: reverse [reverseTemplateFile]." << endl;
@@ -54,16 +54,37 @@ double gasTotal;
 	} // for loop	
 	getline(file,destination);// get the final destination
 	getline(file,location);// which side is the final destination on
-		
-	
-	
-// below here prints out the directions	
-	cout << "How much does your gas (in/per gallons)? " << cost<< endl;
-	cout << "What is your gas mileage (in mpg)? " << mpg << endl;
+	// thid takes the directions and flips them to be backwards.
+	for(int j=0; j<=NumDirection;j++){
+		if (direction[j] == "Right" || direction[j] == "R"){
+			direction[j]= 'L';
+		} // end if
+		else direction[j] = 'R';
+	}// end for	
+	// this turns the ending direction to the opposite
+	if (location == "Right" || location == "R"){
+		location = 'L';
+	} // end if
+	else location = 'R';
+	// below here prints out the directions	
+	cout << "How much does your gas (in/per gallons)? " ;
+	cin >> cost;
+	cout << "What is your gas mileage (in mpg)? ";
+	cin >> mpg;;
 	cout << "Start location " << destination << endl;	
-	for(int a = 0; a < NumDirection ; a++){	
+	//prints out the directions in reverse order
+	for(int a = NumDirection-1; a >= 0 ; a--){	
 		cout << direction[a] << " on " << streetName[a] << " ("<< mileage[a] << " miles)" << endl;	
 	} //end for
-	cout << destination << "is on the " << location << endl;
-	cout << "Your " << mile << "-mile round trip will cost you " << gasTotal << endl;
+	cout << UMW << " is on the " << location << endl;
+	// this turns the mile string in to a double to do math with!
+	for(int m =0; m<=NumDirection; m++){ 
+		mile=atof(mileage[m].c_str());
+		mileTotal= (mile+mileTotal);
+	} // end for
+	mileTotal= mileTotal*2; // this multiples the miles by 2 to make the round trip
+	gasTotal = (mileTotal/mpg)*cost; // calcs the ~gas price
+
+	cout << "Your " << mileTotal << "-mile round trip will cost you $";
+	cout << setprecision(3) << gasTotal << endl;
 }// end of main
